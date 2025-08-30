891cad60b00e43b79472f02e3eb3aba94573259d
Sirjobe <kalenyuk1998@gmail.com>
2025-08-30 17:24:09 +0300

    Перед внесением изменений в class TaskService в метод assign

---
+++ src/main/java/com/javarush/jira/common/internal/config/EnvConfig.java
@@ -0,0 +1,24 @@
+package com.javarush.jira.common.internal.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@Configuration
+public class EnvConfig {
+
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (System.getProperty(key) == null) {
+                System.setProperty(key, value);
+            }
+        });
+    }
+}
---
+++ src/test/java/com/javarush/jira/common/internal/config/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package com.javarush.jira.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Profile("!test")
+    public DataSource postgresDataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource h2DataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;NON_KEYWORDS=VALUE");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+}
